178fe5554b28b02549c79b3e72732c666c338ef5
Alexey Samoylov <alexey.samoylov@gmail.com>
2022-01-12 13:22:45 +0700

    added generic amqp consumer
    Signed-off-by: Alexey Samoylov <alexey.samoylov@gmail.com>

--- pkg/queue/amqp/service.go
+++ pkg/queue/amqp/service.go
@@ -1,11 +1,15 @@
 package amqp
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/isayme/go-amqp-reconnect/rabbitmq"
 	"github.com/streadway/amqp"
 	"grader/pkg/queue"
+	"grader/pkg/workerpool"
+	"reflect"
+	"runtime"
 )
 
 var _ queue.Topic = (*Topic)(nil)
@@ -83,12 +87,14 @@ func (s *Service) Topic(topic string) (queue.Topic, error) {
 
 	return &Topic{
 		exchangeName: topic,
+		queueName:    queueName,
 		channel:      s.Channel(),
 	}, nil
 }
 
 type Topic struct {
 	exchangeName string
+	queueName    string
 	channel      *rabbitmq.Channel
 }
 
@@ -108,4 +114,70 @@ func (t *Topic) Publish(message interface{}) error {
 	}
 
 	return nil
+}
+
+// Consume message from topic
+func (t *Topic) Consume(targetMsg interface{}, consumer queue.ConsumerFunc) error {
+	v := reflect.ValueOf(targetMsg)
+	if v.Kind() != reflect.Ptr {
+		return fmt.Errorf("want msgType as pointer to struct")
+	}
+
+	v = v.Elem() // dereference pointer
+	if v.Kind() != reflect.Struct {
+		return fmt.Errorf("want msgType as pointer to struct")
+	}
+
+	msgType := v.Type()
+
+	messages, err := t.channel.Consume(
+		t.queueName,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return fmt.Errorf("consume: %w", err)
+	}
+
+	pool := workerpool.New()
+	pool.Start(runtime.GOMAXPROCS(0) * 2)
+
+	for amqpMsg := range messages {
+		pool.Run(processMessage(amqpMsg, msgType, consumer))
+	}
+
+	pool.Stop()
+
+	return nil
+}
+
+func processMessage(amqpMsg amqp.Delivery, msgType reflect.Type, consumer queue.ConsumerFunc) workerpool.Job {
+	return func(ctx context.Context) error {
+		v := reflect.New(msgType)
+
+		if err := json.Unmarshal(amqpMsg.Body, &v); err != nil {
+			if err := amqpMsg.Ack(false); err != nil {
+				return fmt.Errorf("amqp acknowledge: %w", err)
+			}
+			return fmt.Errorf("json decode: %w", err)
+		}
+
+		if err := consumer(v); err != nil {
+			if err := amqpMsg.Reject(true); err != nil {
+				return fmt.Errorf("amqp reject: %w", err)
+			}
+			return fmt.Errorf("consumer: %w", err)
+		}
+
+		if err := amqpMsg.Ack(false); err != nil {
+			return fmt.Errorf("amqp acknowledge: %w", err)
+		}
+
+		return nil
+	}
 }
--- pkg/queue/interface.go
+++ pkg/queue/interface.go
@@ -4,6 +4,9 @@ type Queue interface {
 	Topic(topic string) (Topic, error)
 }
 
+type ConsumerFunc func(message interface{}) error
+
 type Topic interface {
 	Publish(message interface{}) error
+	Consume(target interface{}, consumer ConsumerFunc) error
 }
